caf469f85e12f89bfe33de68c5a62c9aa12b1453
lihai2333 <lihai2333@hotmail.com>
2020-06-25 16:44:06 +0800

    做了很多更改

--- src/google-script-App/main.js
+++ src/google-script-App/main.js
@@ -21,15 +21,4 @@ function doPost(e){
   UrlFetchApp.fetch(api, data);
 }
 
-function mainPayload(body) {
 
-	if (body.message.text) {//判断是否是文本 Is the judgment text
-		var payload = {
-			"method": "sendMessage",//方法 method
-			"chat_id": body.message.chat.id,//群组 group 
-			"text": ''//内容 content
-		}
-	return payload;//返回 return
-	}
-
-}
