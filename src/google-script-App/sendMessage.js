5a71ed36d30e380cd891cdc7d454c19d9d9a63f6
lihai2333 <lihai2333@hotmail.com>
2020-06-25 16:16:35 +0800

    加了些判断

--- src/google-script-App/sendMessage.js
+++ src/google-script-App/sendMessage.js
@@ -23,5 +23,13 @@ function doPost(e){
 
 function mainPayload(body) {
 
+	if (body.message.text) {//判断是否是文本 Is the judgment text
+		var payload = {
+			"method": "sendMessage",//方法 method
+			"chat_id": body.message.chat.id,//群组 group 
+			"text": ''//内容 content
+}
+	return payload;
+}
 
 }
