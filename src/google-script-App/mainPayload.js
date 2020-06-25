2dedc70c7059fab99b98cd51e33bf400830cf162
lihai2333 <lihai2333@hotmail.com>
2020-06-25 16:39:50 +0800

    删除了文件

--- src/google-script-App/mainPayload.js
+++
@@ -1,5 +0,0 @@
-function mainPayload(body) {                                                                                if (body.message.text) {//判断是否是文本 I
-s the judgment text
-                var payload = {                                           "method": "sendMessage",//方法 method                                                               "chat_id": body.message.chat.id,//群组 group
-                        "text": ''//内容 content                  }
-        return payload;//返回 return                      }                                                                                           }
