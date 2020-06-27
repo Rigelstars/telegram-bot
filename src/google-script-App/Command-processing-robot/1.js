4c80db287f1f30f223711b5937853cc3c9af74e0
lihai2333 <lihai2333@hotmail.com>
2020-06-27 20:02:37 +0800

    处理命令的机器人

---
+++ src/google-script-App/Command-processing-robot/1.js
@@ -0,0 +1,36 @@
+function doGet(e){
+	  return HtmlService.createHtmlOutput("Hello World");
+}
+
+
+function doPost(e){
+	  var body = JSON.parse(e.postData.contents);
+
+	  body.message.chat.id = body.message.chat.id + '';
+
+	  var payload = mainPayload(body);
+	  var data = {
+		      "method": "post",
+		      "payload": payload
+		    }
+
+	  var botapi = ''//your bot api
+	  var api = 'https://api.telegram.org/bot' + botapi
+	  UrlFetchApp.fetch(api, data);
+}
+
+function mainPayload(body) {
+	let text = body.message.text;
+	let chatId = body.message.chat.id;
+	let messageId = body.message.message_id;
+	
+	var payload = {
+		"method": "sendMessage",
+		"chat_id": chatId
+	}
+
+	if (text == '/start') {
+		payload.text = "本机器人是开源机器人\n\nThis robot is an open source machie people";
+		return payload
+	}
+}
